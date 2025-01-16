66b8f24640c3f87598b794a85f0ec4f253bd016f
Tasnim <tasnimalbrashdi@gmail.com>
2025-01-16 12:05:15 +0400

    Add product repository and service

---
+++ Data/Repositories/ProductRepo.cs
@@ -0,0 +1,74 @@
+﻿namespace AmazonSimulatorApp.Data.Repositories
+{
+    public class ProductRepo : IProductRepo
+    {
+        public ApplicationDbContext _context;
+        public ProductRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            try
+            {
+                return _context.Products.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Database error: {ex.Message}");
+            }
+        }
+
+        public Product GetProductById(int pid)
+        {
+            try
+            {
+                return _context.Products.FirstOrDefault(p => p.PID == pid);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Database error: {ex.Message}");
+            }
+        }
+
+        public void AddProduct(Product product)
+        {
+            try
+            {
+                _context.Products.Add(product);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Database error: {ex.Message}");
+            }
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            try
+            {
+                _context.Products.Update(product);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Database error: {ex.Message}");
+            }
+        }
+
+        public Product GetProductByName(string productName)
+        {
+            try
+            {
+                return _context.Products.FirstOrDefault(p => p.Name == productName);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Database error: {ex.Message}");
+            }
+
+        }
+    }
+}
