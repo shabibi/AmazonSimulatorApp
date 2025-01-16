04c96d0f3f7d14d7bcf83680f8160ca5970e91d1
Tasnim <tasnimalbrashdi@gmail.com>
2025-01-16 13:26:04 +0400

    Add Dtos prouduct

---
+++ Data/DTOs/ProductInputDto.cs
@@ -0,0 +1,13 @@
+﻿namespace AmazonSimulatorApp.Data.DTOs
+{
+    public class ProductInputDto
+    {
+        public int SID { get; set; } // Foreign Key for Seller
+        public int CatID { get; set; } // Foreign Key for Category
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public int Stock { get; set; } // Added stock property
+    }
+}
